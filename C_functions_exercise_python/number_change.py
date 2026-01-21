8d836c20c6baaf0337fef62e9cef05cd4925aa36
unknown <4030079@myuwc.ac.za>
2026-01-21 21:39:59 +0200

    add

---
+++ C_functions_exercise_python/number_change.py
@@ -0,0 +1,14 @@
+#If number is **even**, return **half**
+#If number is **odd**, return **double**
+
+def number_change(num):
+    if num % 2 == 0:
+        return num // 2
+    else:
+        return num * 2
+
+print(number_change(6))   # 3
+print(number_change(7))   # 14
+print(number_change(16))  # 8
+print(number_change(21))  # 42
+
