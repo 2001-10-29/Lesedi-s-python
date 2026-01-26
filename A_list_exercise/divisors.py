b4e5f1db1a870de9ac907d41bbff7da7e71bf65c
unknown <4030079@myuwc.ac.za>
2026-01-26 16:23:45 +0200

    add

---
+++ A_list_exercise/divisors.py
@@ -0,0 +1,13 @@
+# Write a function `divisors(num)` that accepts a number.
+# The function should return a list containing all positive numbers that divide num exactly.
+
+def divisors(num):
+    divs = []
+    for i in range (1, num + 1):
+        if num % i == 0:
+            divs.append(i)
+    print (divs)
+
+divisors(15) #-> [1, 3, 5, 15]
+divisors(7) #-> [1, 7]
+divisors(24) #-> [1, 2, 3, 4, 6, 8, 12, 24]
