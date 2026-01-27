7e2c5170c1e54c3b6f2733f5c6581fa569c8ba22
unknown <4030079@myuwc.ac.za>
2026-01-27 16:07:13 +0200

    add

---
+++ B_lists_exercise/your_average_function.py
@@ -0,0 +1,18 @@
+# Write a function `your_average_function(numbers)` that accepts a list of numbers.
+# The function should return the average of all elements in the list.
+# If the list is empty, the function should return None.
+
+def your_average_function(numbers):
+    if len(numbers) == 0:
+        print('None')
+    else:
+        average = sum(numbers) / len(numbers)
+        print(average)
+
+        
+     
+your_average_function([5, 2, 7, 24]) #-> 9.5
+your_average_function([100, 6]) #-> 53
+your_average_function([31, 32, 40, 12, 33]) #-> 29.6
+your_average_function([]) #-> None 
+
