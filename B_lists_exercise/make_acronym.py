612beb366efdc46482343505e93eea5538c9b01c
unknown <4030079@myuwc.ac.za>
2026-01-27 15:02:00 +0200

    add

---
+++ B_lists_exercise/make_acronym.py
@@ -0,0 +1,15 @@
+# Write a function `make_acronym(sentence)` that accepts a string containing a sentence.
+# The function should return a string containing the first character of each word in the sentence.
+
+def make_acronym(sentence):
+    words = sentence.split()
+    acronym = ''.join(word [0].upper() for word in
+                      words)
+    print (acronym)
+
+
+make_acronym("New York") #-> 'NY'
+make_acronym("same stuff different day") #-> 'SSDD'
+make_acronym("Laugh out loud") #-> 'LOL'
+make_acronym("don't over think stuff") #-> 'DOTS'
+
