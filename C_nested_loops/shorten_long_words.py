a791eb9724edbc0ab9d38b5e039d62ca0c802aa8
unknown <4030079@myuwc.ac.za>
2026-02-05 15:50:26 +0200

    add

---
+++ C_nested_loops/shorten_long_words.py
@@ -0,0 +1,24 @@
+# Write a function `shorten_long_words(sentence)` that accepts a string and returns
+# the same sentence where words longer than 4 characters have their vowels removed.
+
+
+def shorten_Long_words(sentence):
+    def remove_vowels(word):
+        vowels = 'aeiouAEIOU'
+        result = ''
+        for char in word:
+            if char not in vowels:
+                result += char
+    return result 
+    words = sentence.split()
+    shorten_Long_words = []
+    for word in words:
+        if len(word) > 4:
+            shorten_Long_words.append(remove_vowels(word))
+        else:
+            shorten_Long_words.append(word)
+    return ' '.join(shorten_Long_words)
+
+print (shorten_long_words("they are very noble people"))  # 'they are very nbl ppl'
+print (shorten_long_words("stick with it"))  # 'stck with it'
+print (shorten_long_words("ballerina, you must have seen her"))  # 'bllrna, you must have seen her'
