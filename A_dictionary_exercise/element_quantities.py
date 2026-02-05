ae17d865526f89cd1ba584a412425a407d912d2f
unknown <4030079@myuwc.ac.za>
2026-02-05 16:10:16 +0200

    add

---
+++ A_dictionary_exercise/element_quantities.py
@@ -0,0 +1,14 @@
+def element_quantities(dict1):
+    result = []
+    for key, value in dict1.items():
+        result.extend([key] * value)
+    return result
+
+quantities1 = {"cat":3,"bird":1,"dog":2 }
+print(element_quantities(quantities1))
+# ['cat', 'cat', 'cat', 'bird', 'dog', 'dog']
+
+quantities2 = {"blue":3,"brown":1 }
+print(element_quantities(quantities2))
+# ['blue', 'blue', 'blue', 'brown']
+
