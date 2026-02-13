6eb35ae0a12fff2c549af1eb89816e50150410fc
unknown <4030079@myuwc.ac.za>
2026-02-13 07:23:06 +0200

    add

---
+++ B_dictionary_exercise/letter_map.py
@@ -0,0 +1,18 @@
+def letter_map (string, mapping):
+    result = ""
+    for char in string:
+        if char in mapping:
+            result += mapping[char]
+        else:
+            result += char 
+    return result
+
+print(letter_map("symbolic", {"y":"i","o":"a","c":"k" }))
+# 'simbalik'
+
+print(letter_map("colossal", {"o":"x","s":"p" }))
+# 'cxlxppal'
+
+print(letter_map("miniscule", {"u":"t","i":"f","e":"q" }))
+# 'mfnfsctlq'
+
